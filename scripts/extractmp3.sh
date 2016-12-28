4f7d94c1454a08d9d0e14f1fa17519c563709309
Daniel González <gonzalez.lopez.daniel@gmail.com>
2016-12-28 12:10:48 +0100

    Scripts only generate non existing files

--- scripts/extractmp3.sh
+++ scripts/extractmp3.sh
@@ -3,7 +3,9 @@ OIFS="$IFS"
 IFS="\n" 
 
 for file in $*; do
-    ffmpeg -i $file -af "pan=mono|c0=FL" ${file%.*}.mp3   
+    if [ ! -f ${file%.*}.mp3 ]; then
+        ffmpeg -i $file -af "pan=mono|c0=FL" ${file%.*}.mp3   
+    fi
     # echo ${file%.*}
 done
 
--- scripts/trec2mp4.sh
+++ scripts/trec2mp4.sh
@@ -3,7 +3,9 @@ OIFS="$IFS"
 IFS="\n"
 for file in $*; do
     # ffmpeg -i $file -af "pan=mono|c0=FL" ${file%.*}.mp3   
-    ffmpeg -i "$file" -c:a mp3 -c:v libx264 -r 25 "${file%.*}".mp4
+    if [ ! -f "${file%.*}.mp4" ]; then
+        ffmpeg -i "$file" -c:a mp3 -c:v libx264 -r 25 "${file%.*}".mp4
+    fi
     # echo ${file%.*}
 done
 IFS="$OIFS" 
